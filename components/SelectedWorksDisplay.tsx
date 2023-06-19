67b8b1f07b95dd627d8406867b20d0ecdba31c5a
yafet-123 <yafetaddisu123@gmail.com>
2023-06-19 09:11:51 -0700

    adding the selected works database and files

--- components/SelectedWorksDisplay.tsx
+++ components/SelectedWorksDisplay.tsx
@@ -10,7 +10,7 @@ export default function SelectedWorksDisplay({selectedWorks}) {
   return (
   	<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 px-5 lg:px-20 mt-32">
       {selectedWorks.map((data,index)=>(
-            <Link href="/" className="flex flex-col items-center" key={data.asset_id}>
+            <Link href="/" className="flex flex-col items-center" key={data._id}>
               <div className="relative h-[20rem] w-full">
                 <Image
                   src={data.image}
