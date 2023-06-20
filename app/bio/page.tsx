3f6c71b0eed22ebce072e14546068fe88f183538
yafet-123 <yafetaddisu123@gmail.com>
2023-06-20 13:17:42 -0700

    editing the bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -21,11 +21,11 @@ export default async function Bio() {
           <h2 className="font-extrabold tracking-wide text-3xl lg:text-5xl my-5 text-center text-[#355e3b]">ARTIST</h2>
           <h1 className="font-extrabold text-4xl mb-5 text-[#355e3b]">Biography</h1>
           <p className="text-lg lg:text-2xl writing-vertical text-vertical text-left flex flex-col justify-between items-center mb-10">
-            <span className="lg:pr-10">{`Helen Zeray's artistic journey is one that is filled with wonder and admiration for the beauty of nature.
+            <span className="">{`Helen Zeray's artistic journey is one that is filled with wonder and admiration for the beauty of nature.
             Growing up in Addis Abeba, Ethiopia, she was captivated by the artistry of the world around her, which led her to pursue her passion for the arts through private classes in her youth.
             With a heart full of awe and reverence for the natural world, Helen creates stunning works of art that amplify the shapes, colors, and movements of the landscape. 
             Through her use of acrylic, she masterfully captures the essence of nature, revealing the hand of the divine in its splendor.`}</span>
-            <span className="lg:pl-10 mt-10 lg:mt-0">{`But Helen's artistry is not limited to the canvas. She is constantly seeking to expand her knowledge and philosophical understanding of the world beyond her artistic practice. 
+            <span className=" mt-10 lg:mt-0">{`But Helen's artistry is not limited to the canvas. She is constantly seeking to expand her knowledge and philosophical understanding of the world beyond her artistic practice. 
             This thirst for knowledge drives her to explore new compositions, geometric shapes, forms, and colors, as she strives to deepen her connection with the natural world.
             In Helen's commitment to nonfigurative studies and her endless experimentation with techniques and forms, one can see a beautiful example of an artist's journey of growth and discovery. 
             Her passion for the arts and her reverence for nature are a testament to the transformative power of creativity and the wonders of the world around us.`}</span>
