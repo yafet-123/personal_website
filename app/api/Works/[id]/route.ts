f0bc2353cc53d975b529861bf75542e1ac746c2d
yafet-123 <yafetaddisu123@gmail.com>
2023-06-20 12:21:05 -0700

    adding the selected work component page

--- app/api/Works/[id]/route.ts
+++ app/api/Works/[id]/route.ts
@@ -7,14 +7,7 @@ export const GET = async (request, { params }) => {
 
         const individualwork = await Works.findById(params.id).populate("_id")
         if (!individualwork) return new Response("Works Not Found", { status: 404 });
-        const formattedWorks = individualwork.map((work) => {
-            return {
-                ...work.toObject(),
-                _id: work._id.toString()
-            };
-        });
-
-        return new Response(JSON.stringify(formattedWorks), { status: 200 })
+        return new Response(JSON.stringify(individualwork), { status: 200 })
 
     } catch (error) {
         return new Response("Internal Server Error", { status: 500 });
