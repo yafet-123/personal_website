2180944a8646b311a2323ea6d3c9f009c6fe82df
yafet-123 <yafetaddisu123@gmail.com>
2023-06-20 12:19:16 -0700

    adding the selected work component page

--- app/api/Works/route.ts
+++ app/api/Works/route.ts
@@ -6,14 +6,7 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
         await connectToDB()
 
         const works = await Works.find({}).populate('_id')
-        const formattedWorks = works.map((work) => {
-            return {
-                ...work.toObject(),
-                _id: work._id.toString()
-            };
-        });
-        console.log(formattedWorks)
-        return new Response(JSON.stringify(formattedWorks), { status: 200 })
+        return new Response(JSON.stringify(works), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all works", { status: 500 })
     }
