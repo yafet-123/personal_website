b27f86eea0543b04efa642fb9c3735749890b777
yafet-123 <yafetaddisu123@gmail.com>
2023-06-20 11:22:01 -0700

    adding the selected works individual page

--- app/SelectedWorks/[id]/page.tsx
+++ app/SelectedWorks/[id]/page.tsx
@@ -1,33 +1,33 @@
-import Image from "next/image";
-import SelectedWorksIndividualDisplay from '@/components/SelectedWorksIndividualDisplay'
+// import Image from "next/image";
+// import SelectedWorksIndividualDisplay from '@/components/SelectedWorksIndividualDisplay'
 
-const fetchIndividualWorks = async(id : string) => {
-  const data = await fetch(process.env.URL + `/api/Works/${id}`,
-    {   
-      next: {
-        revalidate: 60,
-      },
-    }
-  );
-  const work = await data.json();
-  return work
-}
+// const fetchIndividualWorks = async(id : string) => {
+//   const data = await fetch(process.env.URL + `/api/Works/${id}`,
+//     {   
+//       next: {
+//         revalidate: 60,
+//       },
+//     }
+//   );
+//   const work = await data.json();
+//   return work
+// }
 
-export default async function SelectedWorksView({params : {id} }) {
-  const work  = await fetchIndividualWorks(id);
-  console.log(id)
-  return (
-    <section className="w-full h-full lg:pt-24">
-      <SelectedWorksIndividualDisplay work={work}/>
-    </section>
-  );
-}
+// export default async function SelectedWorksView({params : {id} }) {
+//   const work  = await fetchIndividualWorks(id);
+//   console.log(id)
+//   return (
+//     <section className="w-full h-full lg:pt-24">
+//       <SelectedWorksIndividualDisplay work={work}/>
+//     </section>
+//   );
+// }
 
-export async function generateStaticParams() {
-  const selectedworksview = await fetch(process.env.URL + '/api/Works').then((res) => res.json());
+// export async function generateStaticParams() {
+//   const selectedworksview = await fetch(process.env.URL + '/api/Works').then((res) => res.json());
  
-  return selectedworksview.map((work) => ({
-    worksId: work._id.toString(),
-  }));
-}
+//   return selectedworksview.map((work) => ({
+//     worksId: work._id.toString(),
+//   }));
+// }
 
