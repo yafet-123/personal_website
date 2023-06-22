2e92807449ed6364619f7827763972de1c06614a
yafet-123 <yafetaddisu123@gmail.com>
2023-06-22 07:46:14 -0700

    fixing the user page

--- components/exhibitions/List.tsx
+++ components/exhibitions/List.tsx
@@ -5,7 +5,7 @@ export default function List({exhibitions}) {
   return (
   	<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 px-5 lg:px-20 mt-32">
       {exhibitions.map((data,index)=>(
-        <DisplayForExhibition key={data._id} exhibition={data} />    
+        <Display key={data._id} exhibition={data} />    
       ))}
     </div>
   )
