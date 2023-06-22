484dfc55c7986ba9456ceb682d0d1ea31b4865d8
yafet-123 <yafetaddisu123@gmail.com>
2023-06-22 09:12:49 -0700

    fixing the admin navbar

--- components/Admin/Common/Navbar.tsx
+++ components/Admin/Common/Navbar.tsx
@@ -114,92 +114,92 @@ const Navbar = () => {
               ))}
             </ul>
           </div>
+        </div>
 
-          {/* Desktop Navigation */}
-          <div className="sm:flex hidden">
-            {session?.user ? (
-              <div className="flex gap-3 md:gap-5">
-                <button type="button" onClick={signOut} className="outline_btn">
-                  Sign Out
-                </button>
+        {/* Desktop Navigation */}
+        <div className="sm:flex hidden">
+          {session?.user ? (
+            <div className="flex gap-3 md:gap-5">
+              <button type="button" onClick={signOut} className="outline_btn">
+                Sign Out
+              </button>
 
-                <Link href="/profile">
-                  <Image
-                    src={session?.user.image}
-                    width={37}
-                    height={37}
-                    className="rounded-full"
-                    alt="profile"
-                  />
-                </Link>
-              </div>
-            ) : (
-              <>
-                {providers &&
-                  // bring the providers then list them  in this particular example it is only one
-                  Object.values(providers).map((provider) => (
-                    <button
-                      type="button"
-                      key={provider.name}
-                      onClick={() => {
-                        signIn(provider.id);
-                      }}
-                      className="black_btn"
-                    >
-                      Sign in
-                    </button>
-                  ))}
-              </>
-            )}
-          </div>
-
-          {/* Mobile Navigation */}
-          <div className="sm:hidden flex relative">
-            {session?.user ? (
-              // in this particular example when we click the icon in this example the photo from google icon
-              <div className="flex">
+              <Link href="/profile">
                 <Image
                   src={session?.user.image}
                   width={37}
                   height={37}
                   className="rounded-full"
                   alt="profile"
-                  onClick={() => setToggleDropdown(!toggleDropdown)}
                 />
+              </Link>
+            </div>
+          ) : (
+            <>
+              {providers &&
+                // bring the providers then list them  in this particular example it is only one
+                Object.values(providers).map((provider) => (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => {
+                      signIn(provider.id);
+                    }}
+                    className="black_btn"
+                  >
+                    Sign in
+                  </button>
+                ))}
+            </>
+          )}
+        </div>
 
-                {toggleDropdown && (
-                  <div className="dropdown">
-                    <button
-                      type="button"
-                      onClick={() => {
-                        setToggleDropdown(false);
-                        signOut();
-                      }}
-                      className="mt-5 w-full black_btn"
-                    >
-                      Sign Out
-                    </button>
-                  </div>
-                )}
-              </div>
-            ) : (
-              <>
-                {providers &&
-                  Object.values(providers).map((provider) => (
-                    <button
-                      type="button"
-                      key={provider.name}
-                      onClick={() => {
-                        signIn(provider.id);
-                      }}
-                      className="black_btn"
-                    >
-                      Sign in
-                    </button>
-                  ))}
-              </>
-            )}
-          </div>
+        {/* Mobile Navigation */}
+        <div className="sm:hidden flex relative">
+          {session?.user ? (
+            // in this particular example when we click the icon in this example the photo from google icon
+            <div className="flex">
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+                onClick={() => setToggleDropdown(!toggleDropdown)}
+              />
+
+              {toggleDropdown && (
+                <div className="dropdown">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setToggleDropdown(false);
+                      signOut();
+                    }}
+                    className="mt-5 w-full black_btn"
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              {providers &&
+                Object.values(providers).map((provider) => (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => {
+                      signIn(provider.id);
+                    }}
+                    className="black_btn"
+                  >
+                    Sign in
+                  </button>
+                ))}
+            </>
+          )}
         </div>
       </div>
     </nav>
