c32ea71d3708539550efb8452245a316730a7111
yafet-123 <yafetaddisu123@gmail.com>
2023-06-23 08:38:55 -0700

    adding works page for admin

--- components/Admin/Works/Display.tsx
+++ components/Admin/Works/Display.tsx
@@ -6,15 +6,15 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const Display = ({ blogs, handleEdit, handleDelete }) => {
+const Display = ({ Works, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
   const [copied, setCopied] = useState("");
-  console.log(blogs);
+  console.log(Works);
   const handleCopy = () => {
-    setCopied(blogs.Header);
-    navigator.clipboard.writeText(blogs.Header);
+    setCopied(Works.title);
+    navigator.clipboard.writeText(Works.title);
     setTimeout(() => setCopied(false), 3000);
   };
 
@@ -23,21 +23,21 @@ const Display = ({ blogs, handleEdit, handleDelete }) => {
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-between items-center gap-3 cursor-pointer">
           <h3 className="font-satoshi font-semibold text-gray-900">
-            {blogs.blogs_id}
+            {Works._id}
           </h3>
           <p className="font-inter text-sm text-gray-500">
-            {moment(blogs.ModifiedDate).utc().format("YYYY-MM-DD")}
+            {moment(Works.ModifiedDate).utc().format("YYYY-MM-DD")}
           </p>
         </div>
 
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
-              copied === blogs.Header
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
+              copied === user.email
+                ? "/icons/tick.svg"
+                : "/icons/copy.svg"
             }
-            alt={copied === blogs.Header ? "tick_icon" : "copy_icon"}
+            alt={copied === user.email ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
@@ -46,13 +46,13 @@ const Display = ({ blogs, handleEdit, handleDelete }) => {
 
       <div className="flex justify-between items-center">
         <p className="my-4 font-satoshi text-sm text-gray-700">
-          {blogs.Header}
+          {Works.title}
         </p>
-        <Image src={blogs.Image} alt="blogs Image" width={100} height={100} />
+        <Image src={Works.image} alt="Works Image" width={100} height={100} />
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">
-        {blogs.ShortDescription}
+        {Works.exhibitions}
       </p>
 
       {session?.user.email === "yafetaddisu123@gmail.com" && (
