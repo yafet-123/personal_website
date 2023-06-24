7249544cf46a84995f3875f2ef8d00e401ecce2f
yafet-123 <yafetaddisu123@gmail.com>
2023-06-24 14:36:20 -0700

    fixing json parse error

--- app/api/Exhibitions/Create/route.ts
+++ app/api/Exhibitions/Create/route.ts
@@ -1,16 +1,16 @@
-import exhibition from "@/models/exhibition";
-import { connectToDB } from "@/utils/database";
-import { NextApiRequest, NextApiResponse } from "next";
+// import exhibition from "@/models/exhibition";
+// import { connectToDB } from "@/utils/database";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export const POST = async (request:NextApiRequest) => {
-	const { title ,descreption ,image ,date } = await request.json();
-    try {
-        await connectToDB();
-        const newexhibition = new exhibition({title ,descreption ,image ,date})
+// export const POST = async (request:NextApiRequest) => {
+// 	const { title ,descreption ,image ,date } = await request.json();
+//     try {
+//         await connectToDB();
+//         const newexhibition = new exhibition({title ,descreption ,image ,date})
 
-        await newexhibition.save();
-        return new Response(JSON.stringify(newexhibition), { status: 201 })
-    } catch (error) {
-        return new Response("Failed to create a new prompt", { status: 500 });
-    }
-}
+//         await newexhibition.save();
+//         return new Response(JSON.stringify(newexhibition), { status: 201 })
+//     } catch (error) {
+//         return new Response("Failed to create a new prompt", { status: 500 });
+//     }
+// }
--- app/api/Exhibitions/route.ts
+++ app/api/Exhibitions/route.ts
@@ -1,14 +1,14 @@
-import exhibition from "@/models/exhibition";
-import { connectToDB } from "@/utils/database";
+// import exhibition from "@/models/exhibition";
+// import { connectToDB } from "@/utils/database";
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await connectToDB();
+// export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     await connectToDB();
 
-    const exhibitions = await exhibition.find({}).populate("_id");
+//     const exhibitions = await exhibition.find({}).populate("_id");
 
-    return new Response(JSON.stringify(exhibitions), { status: 200 });
-  } catch (error) {
-    return new Response("Failed to fetch all exhibitions", { status: 500 });
-  }
-};
+//     return new Response(JSON.stringify(exhibitions), { status: 200 });
+//   } catch (error) {
+//     return new Response("Failed to fetch all exhibitions", { status: 500 });
+//   }
+// };
