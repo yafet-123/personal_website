bd899d82883a17e237f157dd7bbfc20e4edab1a0
yafet-123 <yafetaddisu123@gmail.com>
2023-06-24 14:47:55 -0700

    fixing selcetd works image

--- components/works/Display.tsx
+++ components/works/Display.tsx
@@ -8,7 +8,7 @@ export default function Display({ work }) {
         <h1 className="font-medium tracking-wide text-3xl lg:text-6xl mb-5 lg:mb-10">
           {work.title}
         </h1>
-        <p className="text-xl lg:text-2xl mb-5 lg:mb-10">{work.descreption}</p>
+        <p className="text-xl lg:text-2xl mb-5 lg:mb-10">{work.description}</p>
         <p className="flex flex-col">
           <span className="text-lg my-5">EXHIBITIONS:</span>
           <span className="lg:pl-10 text-lg mb-5">{work.exhibitions}</span>
@@ -16,7 +16,7 @@ export default function Display({ work }) {
       </div>
       <div className="w-full h-96 lg:!h-[35rem] relative justify-self-center mb-5 lg:mb-0">
         <Image
-          src={work.image}
+          src={work.Image}
           fill
           className="!bg-cover w-full !h-full"
           alt="latest news image"
--- components/works/List.tsx
+++ components/works/List.tsx
@@ -3,7 +3,7 @@ export default function List({ selectedWorks }) {
   return (
     <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 px-5 lg:px-20">
       {selectedWorks.map((data, index) => (
-        <Work key={data._id} work={data} />
+        <Work key={data.selectedWorks_id} work={data} />
       ))}
     </div>
   );
