1ede901deb0dcc9412f39689d755b0b63fc332c0
yafet-123 <yafetaddisu123@gmail.com>
2023-06-24 11:17:29 -0700

    adding the api for selected works to give us all the works

--- app/api/Works/Add/route.ts
+++ app/api/Works/Add/route.ts
@@ -1,15 +1,19 @@
-import Works from "@/models/works";
-import { connectToDB } from "@/utils/database";
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/utils/db.server";
 
 export const POST = async (request:NextApiRequest) => {
 	const { title, exhibitions, image,description, user_id  } = await request.json();
     try {
-        await connectToDB();
-    	const newWork = new Works({ title, exhibitions, descreption:description, image, creator:user_id });
-    	console.log(newWork)
-    	await newWork.save();
-        return new Response(JSON.stringify(newWork), { status: 201 })
+        const data = await prisma.SelectedWorks.create({
+          data: {
+            title,
+            exhibitions,
+            Image:image,
+            description,
+            user_id,
+          },
+        });
+        return new Response(JSON.stringify(data), { status: 201 })
     } catch (error) {
         return new Response("Failed to create a new prompt", { status: 500 });
     }
