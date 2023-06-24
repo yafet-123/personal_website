b5d77d405379c65d086d8550bb6564e85332cccf
yafet-123 <yafetaddisu123@gmail.com>
2023-06-24 11:57:47 -0700

    fixing the admin page

--- app/api/Exhibitions/[id]/route.ts
+++ app/api/Exhibitions/[id]/route.ts
@@ -1,23 +1,23 @@
-// import exhibition from "@/models/exhibition";
-// import { connectToDB } from "@/utils/database";
+import exhibition from "@/models/exhibition";
+import { connectToDB } from "@/utils/database";
 
-// export const GET = async (request, { params }) => {
-//   try {
-//     await connectToDB();
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
 
-//     const individualexhibition = await exhibition
-//       .findById(params.id)
-//       .populate("_id");
-//     if (!individualexhibition)
-//       return new Response("exhibition Not Found", { status: 404 });
-//     const formattedexhibition = individualexhibition.map((exhibition) => {
-//       return {
-//         ...exhibition.toObject(),
-//         _id: exhibition._id.toString(),
-//       };
-//     });
-//     return new Response(JSON.stringify(formattedexhibition), { status: 200 });
-//   } catch (error) {
-//     return new Response("Internal Server Error", { status: 500 });
-//   }
-// };
+    const individualexhibition = await exhibition
+      .findById(params.id)
+      .populate("_id");
+    if (!individualexhibition)
+      return new Response("exhibition Not Found", { status: 404 });
+    const formattedexhibition = individualexhibition.map((exhibition) => {
+      return {
+        ...exhibition.toObject(),
+        _id: exhibition._id.toString(),
+      };
+    });
+    return new Response(JSON.stringify(formattedexhibition), { status: 200 });
+  } catch (error) {
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
