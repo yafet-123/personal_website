c631d1a5caaff33f9bc7bb29ef85f7d96e74710a
yafet-123 <yafetaddisu123@gmail.com>
2023-06-24 23:49:09 -0700

    fixing the selected works for the client page

--- app/api/Works/route.ts
+++ app/api/Works/route.ts
@@ -24,7 +24,7 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
       UserName: data.User.UserName,
     }));
     
-    return new Response(JSON.stringify(Allworks), { status: 200 });
+    return new Response(JSON.stringify(works), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all works", { status: 500 });
   }
