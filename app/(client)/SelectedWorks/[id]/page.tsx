22204048573e0355707d194d23f62e5782c57e02
yafet-123 <yafetaddisu123@gmail.com>
2023-06-25 00:40:06 -0700

    adding exhibitions for admin page

--- app/(client)/SelectedWorks/[id]/page.tsx
+++ app/(client)/SelectedWorks/[id]/page.tsx
@@ -32,7 +32,7 @@ export async function generateStaticParams() {
   const selectedworksview = await fetch(process.env.URL + '/api/Works').then((res) => res.json());
   console.log(selectedworksview[1].selectedWorks_id)
   return selectedworksview.map((work) => ({
-    worksId:work.selectedWorks_id.toString(),
+    worksId:work.selectedWorks_id,
   }));
 }
 
