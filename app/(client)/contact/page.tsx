8ccd4078a283b0a63a1e692e64a1988ccaa3e7c2
yafet-123 <yafetaddisu123@gmail.com>
2023-06-29 16:12:24 -0700

    editing the navbar component

--- app/(client)/contact/page.tsx
+++ app/(client)/contact/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 const ContactUsPage = () => {
   return (
       <div className="w-full h-full flex flex-col justify-center items-center px-2 lg:px-10 py-32 bg-white">
-        <h1 className="font-poppins font-bold sm:leading-[52px] text-3xl md:text-5xl leading-[36px] xs:tracking-[-0.5%] tracking-[-0.25%] text-left text-[#010101] mb-5 px-10">
+        <h1 className="font-poppins font-bold sm:leading-[52px] text-4xl lg:tetx-6xl leading-[36px] xs:tracking-[-0.5%] tracking-[-0.25%] text-left text-[#010101] mb-5 px-10">
           Contact
         </h1>
         <ContactForm />
--- components/contactus/ContactForm.jsx
+++ components/contactus/ContactForm.jsx
@@ -57,105 +57,109 @@ const ContactForm = () => {
         validate={validateForm}
         onSubmit={handleSubmit}
       >
-        <Form className="w-full flex flex-col space-y-6">
-          <div className="font-poppins text-left text-[#010101]">
-            <h1 className="font-bold text-3xl lg:tetx-4xl mb-2">Helen Zeray</h1>
-            <p className="font-normal text-xl lg:text-2xl mb-2">Sderot hachmel Israel 32</p>
-            <p className="font-normal text-xl lg:text-2xl mb-2">WhatsApp: +9725353323525</p>
+        <div className="w-full flex flex-col md:flex-row space-y-6">
+          <div className="flex flex-col">
+            <div className="font-poppins text-left text-[#010101]">
+              <h1 className="font-bold text-3xl lg:tetx-4xl mb-2">Helen Zeray</h1>
+              <p className="font-normal text-xl lg:text-2xl mb-2">Sderot hachmel Israel 32</p>
+              <p className="font-normal text-xl lg:text-2xl mb-2">WhatsApp: +9725353323525</p>
+            </div>
+
+            <div className="flex gap-4">
+              {socialMediaLinks.map((paths, index) => {
+                return (
+                  <Link key={index} href={paths.path} target="_blank">
+                    {paths.path}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
 
-          <div className="flex gap-4">
-            {socialMediaLinks.map((paths, index) => {
-              return (
-                <Link key={index} href={paths.path} target="_blank">
-                  {paths.path}
-                </Link>
-              );
-            })}
-          </div>
+          <form className="flex flex-col">
+            <h3 className="font-poppins text-left text-[#010101] font-bold text-4xl lg:tetx-6xl">Enquiry form</h3>
 
-          <h3 className="font-poppins text-left text-[#010101] font-bold text-4xl lg:tetx-6xl">Enquiry form</h3>
+            <div className="mb-4">
+              <label htmlFor="name" className="block mb-1">
+                Name:
+                <span className="text-gray-500 text-sm ml-1">(required)</span>
+              </label>
+              <Field
+                type="name"
+                id="name"
+                name="name"
+                className="w-full p-2 text-black border border-"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="name" className="block mb-1">
-              Name:
-              <span className="text-gray-500 text-sm ml-1">(required)</span>
-            </label>
-            <Field
-              type="name"
-              id="name"
-              name="name"
-              className="w-full p-2 text-black border border-"
-            />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-1">
+                Email:
+                <span className="text-gray-500 text-sm ml-1">(required)</span>
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="w-full p-2 text-black border border-"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-1">
-              Email:
-              <span className="text-gray-500 text-sm ml-1">(required)</span>
-            </label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              className="w-full p-2 text-black border border-"
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="block mb-1">
+                Phone:
+                <span className="text-gray-500 text-sm ml-1">(required)</span>
+              </label>
+              <Field
+                type="text"
+                id="phone"
+                name="phone"
+                className="w-full p-2 text- border border-"
+              />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="phone" className="block mb-1">
-              Phone:
-              <span className="text-gray-500 text-sm ml-1">(required)</span>
-            </label>
-            <Field
-              type="text"
-              id="phone"
-              name="phone"
-              className="w-full p-2 text- border border-"
-            />
-            <ErrorMessage
-              name="phone"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block mb-1">
+                Message:
+                <span className="text-gray-500 text-sm ml-1">(required)</span>
+              </label>
+              <Field
+                as="textarea"
+                id="message"
+                name="message"
+                className="w-full p-2 text-black border border-"
+              />
+              <ErrorMessage
+                name="message"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="message" className="block mb-1">
-              Message:
-              <span className="text-gray-500 text-sm ml-1">(required)</span>
-            </label>
-            <Field
-              as="textarea"
-              id="message"
-              name="message"
-              className="w-full p-2 text-black border border-"
-            />
-            <ErrorMessage
-              name="message"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="bg-[#17c294] w-28 text-white paragraph-fonts py-2 px-4 mb-8 md:mb-0 md:py-4 md:px-8
-                         shadow-black items-center rounded-md justify-center shadow-md hover:scale-105 duration-300"
-          >
-            Submit
-          </button>
-        </Form>
+            <button
+              type="submit"
+              className="bg-[#17c294] w-28 text-white paragraph-fonts py-2 px-4 mb-8 md:mb-0 md:py-4 md:px-8
+                           shadow-black items-center rounded-md justify-center shadow-md hover:scale-105 duration-300"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </Formik>
     </div>
   );
