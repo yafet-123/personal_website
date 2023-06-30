771a1f13cfe212c707242b36520da1f7d2214724
yafet-123 <yafetaddisu123@gmail.com>
2023-06-30 06:04:36 -0700

    fixing the config file

--- app/(client)/News/page.tsx
+++ app/(client)/News/page.tsx
@@ -7,28 +7,29 @@ export const metadata: Metadata = {
   description: "Helen Zeray's artistic journey is one that is filled with wonder and admiration for the beauty of nature.",
 }
 
-// async function fetchNews() {
-//   const response = await fetch(
-//     // fetch from our code repository
-//     process.env.URL + '/api/News',
-//     {   
-//       next: {
-//         revalidate: 60,
-//       },
-//     }
-//   );
-//   console.log(response)
-//   const news = await response.json();
-//   return news;
-// }
+async function fetchNews() {
+  const response = await fetch(
+    // fetch from our code repository
+    process.env.URL + '/api/News',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  console.log(response)
+  const news = await response.json();
+  return news;
+}
 
 export default async function News() {
-  // const news = await fetchNews();
+  const news = await fetchNews();
+  console.log(news)
   return (
     <main className="flex flex-col items-center bg-white">
-      {/*<Suspense fallback={<div className="w-full h-screen flex items-center justify-center text-2xl ">Loading News Please Wait ...</div>}> 
+      <Suspense fallback={<div className="w-full h-screen flex items-center justify-center text-2xl ">Loading News Please Wait ...</div>}> 
         <List selectenews={news} />
-      </Suspense>*/}
+      </Suspense>
     </main>
   )
 }
