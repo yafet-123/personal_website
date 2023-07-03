54a7c7f07d9dfd3cd91332e4bc8959acaf725770
yafet-123 <yafetaddisu123@gmail.com>
2023-07-03 06:07:37 -0700

    adding the middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,19 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   async function middleware(req) {
-    console.log(req.nextauth.token)
+    console.log(req.nextauth.user) // Access the user object
+
+    const userId = req.nextauth.user?.id
+    if (userId) {
+      const user = await prisma.user.findUnique({
+        where: { user_id: userId },
+      })
+      console.log(user) // Access specific user properties
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) => token?.role === "admin",
+      authorized: () => true,
     },
   }
 )
