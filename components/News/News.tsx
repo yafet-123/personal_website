16ad730be7a361c7ac2d03998ead028a2c86287f
yafet-123 <yafetaddisu123@gmail.com>
2023-07-05 04:52:09 -0700

    adding sitemap

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -7,10 +7,10 @@ export default function Work({ news }) {
   return (
     <Link
       href={news.link}
-      className="text-white flex flex-col cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300 border shadow-2xl"
+      className="text-white flex flex-row cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300 border shadow-2xl"
       key={news.news_id}
     >
-      <div className="relative h-[400px] w-full">
+      <div className="relative h-[300px] w-full">
         <Image
           src={news.Image[0]}
           alt="Slide"
@@ -18,14 +18,16 @@ export default function Work({ news }) {
           layout="fill"
         />
       </div>
- 
-      <h1 className="my-[1rem] text-left font-poppins font-semibold text-[1.3rem] text-[#010101] text-center">
-        {news.title}
-      </h1>
 
-      <p className="mb-5 font-poppins xs:text-[20px] xs:leading-[27px] text-[16px] leading-[23px] font-normal w-full text-left text-[#505e66] mt-4">
-        {news.description}
-      </p>
+      <div className="flex flex-col">
+        <h1 className="my-[1rem] text-left font-poppins font-semibold text-[1.3rem] text-[#010101] text-center">
+          {news.title}
+        </h1>
+
+        <p className="mb-5 font-poppins xs:text-[20px] xs:leading-[27px] text-[16px] leading-[23px] font-normal w-full text-left text-[#505e66] mt-4">
+          {news.description}
+        </p>
+      </div>
     </Link>
   );
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.URL || 'https://helen-zeray-website.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
