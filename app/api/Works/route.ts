d86734af63da6b3c6ffbb2b1ef5f1678aca50f5c
yafet-123 <yafetaddisu123@gmail.com>
2023-07-07 04:35:12 -0700

    editing bios page

--- app/api/Works/route.ts
+++ app/api/Works/route.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/utils/db.server";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
