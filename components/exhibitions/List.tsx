bc2e4a3f6af1b3f0689607c2904208bf3d8e915d
yafet-123 <yafetaddisu123@gmail.com>
2023-07-08 10:29:27 -0700

    editing the exhibition view

--- components/exhibitions/List.tsx
+++ components/exhibitions/List.tsx
@@ -5,7 +5,7 @@ export default function List({ exhibitions }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-2 lg:px-10">
       {exhibitions.map((data, index) => (
-        <Work key={data.exhibition_id} exhibition={data} />
+        <Exhibition key={data.exhibition_id} exhibition={data} />
       ))}
     </div>
   );
