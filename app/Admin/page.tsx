4530cc54b114706fbe2593c97a0b6b61356e3280
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 10:44:03 -0700

    fixing error in the admin page

--- app/Admin/page.tsx
+++ app/Admin/page.tsx
@@ -6,18 +6,18 @@ export const metadata: Metadata = {
   title: 'Admin Home',
 }
 
-// async function fetchExhibitionsCount() {
-//   const data = await fetch(
-//     process.env.URL + '/api/Exhibitions/Count',
-//     {   
-//       next: {
-//         revalidate: 60,
-//       },
-//     }
-//   );
-//   const exhibitions = await data.json();
-//   return exhibitions;
-// }
+async function fetchExhibitionsCount() {
+  const data = await fetch(
+    process.env.URL + '/api/Exhibitions/Count',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const exhibitions = await data.json();
+  return exhibitions;
+}
 
 async function fetchuserCount() {
   const data = await fetch(
@@ -32,31 +32,31 @@ async function fetchuserCount() {
   return users;
 }
 
-// async function fetchnewsCount() {
-//   const data = await fetch(
-//     process.env.URL + '/api/News/Count',
-//     {   
-//       next: {
-//         revalidate: 60,
-//       },
-//     }
-//   );
-//   const news = await data.json();
-//   return news;
-// }
+async function fetchnewsCount() {
+  const data = await fetch(
+    process.env.URL + '/api/News/Count',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const news = await data.json();
+  return news;
+}
 
-// async function fetchWorkCount() {
-//   const data = await fetch(
-//     process.env.URL + '/api/Works/Count',
-//     {   
-//       next: {
-//         revalidate: 60,
-//       },
-//     }
-//   );
-//   const works = await data.json();
-//   return works;
-// }
+async function fetchWorkCount() {
+  const data = await fetch(
+    process.env.URL + '/api/Works/Count',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const works = await data.json();
+  return works;
+}
 
 export default async function AdminHome() {
   // const exhibitioncount = await fetchExhibitionsCount();
@@ -71,7 +71,7 @@ export default async function AdminHome() {
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{usercount}</h2>
         </div>
 
-        {/*<div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
+        <div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
           <h1 className="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black pb-5">Works</h1>
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{Workcount}</h2>
         </div>
@@ -84,7 +84,7 @@ export default async function AdminHome() {
         <div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
           <h1 className="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black pb-5">Exhibition</h1>
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{exhibitioncount}</h2>
-        </div>*/}
+        </div>
       </div>
       {/*<BarChart work={Workcount} news={newscount} exhibition={exhibitioncount} user={usercount} />*/}
     </section>
