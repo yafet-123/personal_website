cd284f05bb03aa113d9795496a46e93533ce64d1
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 10:29:22 -0700

    adding chart.js

--- app/api/User/Count/route.ts
+++ app/api/User/Count/route.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = async () => {
   try {
     const counts = await prisma.User.count()
     return new Response(JSON.stringify(counts), { status: 200 });  
