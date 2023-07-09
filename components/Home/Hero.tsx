6947ce2082909dd0a3528b81ed5f21c5c25ae6cb
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 13:52:45 -0700

    editinghero page

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,63 +1,36 @@
 "use client";
-import Slider, { Settings, LazyLoadTypes } from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import {BsFacebook, BsYoutube, BsLinkedin, BsInstagram, BsTwitter, BsTelegram} from 'react-icons/bs'
-import Link from 'next/link'
-const Hero: React.FC = () => {
-  var settings: Settings = {
-    dots: false,
-    lazyLoad: "ondemand" as LazyLoadTypes,
-    fade: true,
-    infinite: true,
-    autoplay: true,
-    speed: 7000,
-    autoplaySpeed: 7000,
- 
-  };
-  const socialMediaLinks = [
-    {id:"https://www.linkedin.com/in/helen-zeray-789b89267",path:<BsLinkedin size={30} color="white"/>},
-    {id:"https://instagram.com/helenzeray1?igshid=ZGUzMzM3NWJiOQ==",path:<BsInstagram size={30} color="white"/>},
-  ]
+import React, { useEffect, useRef } from 'react';
+import Swiper from 'swiper';
+import 'swiper/swiper.min.css';
+
+const MySwiper = () => {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      new Swiper(swiperRef.current, {
+        slidesPerView: 'auto', // Set the number of slides per view
+        spaceBetween: 16, // Set the space between slides
+        grabCursor: true, // Enable grab cursor when user hovers over the swiper
+        pagination: {
+          el: '.swiper-pagination', // Specify the pagination container element
+          clickable: true // Enable clickable pagination bullets
+        }
+      });
+    }
+  }, []);
+
   return (
-    <div className="w-full h-screen overflow-hidden">
-      <Slider {...settings}>
-        {CardHeo.map((data, index) => (
-          <div
-            key={index}
-            className={`${data.image} w-full h-screen bg-fixed relative`}
-          >
-            <div className="absolute top-1/2 flex justify-end -translate-y-1/2 w-full px-10">
-              
-            </div>
-          </div>
-        ))}
-      </Slider>
+    <div ref={swiperRef} className="swiper-container">
+      <div className="swiper-wrapper">
+        {/* Add your slides here */}
+        <div className="swiper-slide">Slide 1</div>
+        <div className="swiper-slide">Slide 2</div>
+        <div className="swiper-slide">Slide 3</div>
+      </div>
+      <div className="swiper-pagination"></div>
     </div>
   );
 };
 
-export default Hero;
-
-const CardHeo = [
-  {
-    id: 1,
-    image: "hero-background-one",
-  },
-  {
-    id: 2,
-    image: "hero-background-two",
-  },
-  {
-    id: 3,
-    image: "hero-background-three",
-  },
-  {
-    id: 4,
-    image: "hero-background-four",
-  },
-  {
-    id: 5,
-    image: "hero-background-five",
-  },
-];
+export default MySwiper;
