0902948d37a515ac3db3aa9d092234c7cf5aa6ac
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 14:27:35 -0700

    editing hero page

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,44 +1,42 @@
 "use client";
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-
-// import required modules
-import { Pagination, Navigation } from 'swiper/modules';
-
-export default function Hero() {
+import Slider, { Settings, LazyLoadTypes } from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {BsFacebook, BsYoutube, BsLinkedin, BsInstagram, BsTwitter, BsTelegram} from 'react-icons/bs'
+import Link from 'next/link'
+const Hero: React.FC = () => {
+  var settings: Settings = {
+    dots: false,
+    lazyLoad: "ondemand" as LazyLoadTypes,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    speed: 7000,
+    autoplaySpeed: 7000,
+ 
+  };
+  const socialMediaLinks = [
+    {id:"https://www.linkedin.com/in/helen-zeray-789b89267",path:<BsLinkedin size={30} color="white"/>},
+    {id:"https://instagram.com/helenzeray1?igshid=ZGUzMzM3NWJiOQ==",path:<BsInstagram size={30} color="white"/>},
+  ]
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="w-full h-full"
-      >
+    <div className="w-full h-screen overflow-hidden">
+      <Slider {...settings}>
         {CardHeo.map((data, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className={`${data.image} w-full h-screen bg-fixed relative`}
-            >
-              
-            </div>
-          </SwiperSlide>
+          <div
+            key={index}
+            className={`${data.image} w-full h-screen bg-fixed relative`}
+          >
+            
+          </div>
         ))}
-      </Swiper>
-    </>
+      </Slider>
+    </div>
   );
-}
+};
+
+export default Hero;
 
 const CardHeo = [
   {
