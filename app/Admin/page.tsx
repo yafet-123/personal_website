1d371f2ad3a953f2e345ab81fdb5b225e5729750
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 10:40:59 -0700

    fixing error in the admin page

--- app/Admin/page.tsx
+++ app/Admin/page.tsx
@@ -6,18 +6,18 @@ export const metadata: Metadata = {
   title: 'Admin Home',
 }
 
-// async function fetchExhibitionsCount() {
-//   const data = await fetch(
-//     process.env.URL + '/api/Exhibitions/Count',
-//     {   
-//       next: {
-//         revalidate: 60,
-//       },
-//     }
-//   );
-//   const exhibitions = await data.json();
-//   return exhibitions;
-// }
+async function fetchExhibitionsCount() {
+  const data = await fetch(
+    process.env.URL + '/api/Exhibitions/Count',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const exhibitions = await data.json();
+  return exhibitions;
+}
 
 // async function fetchuserCount() {
 //   const data = await fetch(
@@ -59,19 +59,19 @@ export const metadata: Metadata = {
 // }
 
 export default async function AdminHome() {
-  // const exhibitioncount = await fetchExhibitionsCount();
+  const exhibitioncount = await fetchExhibitionsCount();
   // const Workcount = await fetchWorkCount();
   // const usercount = await fetchuserCount();
   // const newscount = await fetchnewsCount();
   return (
     <section className="mt-32 flex flex-col w-full h-full">
-      {/*<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-2 lg:px-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-2 lg:px-10">
         <div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
           <h1 className="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black pb-5">User</h1>
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{usercount}</h2>
         </div>
 
-        <div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
+        {/*<div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
           <h1 className="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black pb-5">Works</h1>
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{Workcount}</h2>
         </div>
@@ -84,9 +84,9 @@ export default async function AdminHome() {
         <div className="flex flex-col items-center text-white p-[2rem] rounded-[20px] bg-[#F7F7F7] cursor-pointer shadow-md hover:shadow-xl shadow-inherit overflow-hidden hover:scale-95 transition-transform duration-300" >
           <h1 className="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black pb-5">Exhibition</h1>
           <h2 className="font-normal font-poppins xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] text-black">{exhibitioncount}</h2>
-        </div>
+        </div>*/}
       </div>
-      <BarChart work={Workcount} news={newscount} exhibition={exhibitioncount} user={usercount} />*/}
+      {/*<BarChart work={Workcount} news={newscount} exhibition={exhibitioncount} user={usercount} />*/}
     </section>
   );
 }
