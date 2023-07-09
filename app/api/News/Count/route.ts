3008c4f50b87d83212d12b8da7d08a3bd051eb32
yafet-123 <yafetaddisu123@gmail.com>
2023-07-09 09:16:17 -0700

    adding exhibition count

---
+++ app/api/News/Count/route.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const counts = await prisma.news.count()
+    return new Response(JSON.stringify(counts), { status: 200 });  
+	} catch (error) {
+    return new Response("Failed to fetch news Count", { status: 500 });
+  }
+};
